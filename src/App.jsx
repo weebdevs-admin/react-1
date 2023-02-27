7d846aaf6e4c64b558f1a2ac26890c909f44049a
weebhacker <weebhacker1@gmail.com>
2023-02-27 12:11:52 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react';
 import './App.css';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
-
 function App() {
-  const [ count, setCount]= useState(0)
+  const [counter, setCounter]= useState(0)
+  console.log(counter+setCounter);
   return (
     <div className="App">
       
-      <Header data={setCount} count={count}/>
-      <Footer count={count}/>
+      <Header />
+      <Footer />
     </div>
   );
 }
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.scss'
+import moon from './../assets/image/moon.svg'
+import instagram from './../assets/image/instagram.svg'
+import telegram from './../assets/image/telegram.svg'
+import linkedin from './../assets/image/linkedin.svg'
 
-function Header({data, count}) {
+function Header() {
+  const [dark, setDark]= useState(true)
+  const handler = ()=>{
+    setDark(!dark)
+  }
   return (
-    <div className='Header'>
-        <button className='btn' onClick={()=>data(count+1)}>+</button>
-        <button className='btn' onClick={()=>data(count>0?count-1: 0)}>-</button>
+    
+    <div className={dark?'Header bglight':'Header bgdark'}>
+        <nav className={dark?'navbar container bglight':'navbar container NavbarShadow bgdark'}>
+          <ul>
+            <li><a className={dark?'item dark': 'item light'} href="/">main</a></li>
+            <li><a className={dark?'item dark': 'item light'} href="/">abaut me</a></li>
+            <li><a className={dark?'item dark': 'item light'} href="/">help</a></li>
+            <li><a className={dark?'item dark': 'item light'} href="/">contact us</a></li>
+            <li><img onClick={handler} src={moon} alt="" /></li>
+            <li><h1 className={dark? " dark":" NavbarLogo"}>WeeB.Dev</h1></li>
+          </ul>
+        </nav>
+        <div className="abaut">
+          <ul className="abaut__list container">
+            <li className="abaut__item">
+              <h2 className={dark?'abaut__title ':'abaut__title NavbarLogo'}>WeeB DevolopmeNT</h2>
+              <p className={dark?'abaut__desc':'abaut__desc NavbarLogo'}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptatem architecto molestiae voluptate at ullam laudantium blanditiis consequatur hic nostrum? Itaque omnis est aperiam facilis provident, atque dolorem quas. Quibusdam.</p><br />
+              <button className="abaut__btn">contact us</button>
+            </li>
+            <li className={dark?'abaut__item2 ':'abaut__item2 NavbarShadow'}></li>
+          </ul>
+        </div>
+        <ul className="icon">
+          <li><a href="https://instagram.com/weeb.dev"><img src={instagram} alt="" /></a></li>
+          <li><a href="https://www.linkedin.com/in/weebdev-weeb-dev-73928a25b/"><img src={linkedin} alt="" /></a></li>
+          <li><a href="https://t.me/weebuz"><img src={telegram} alt="" /></a></li>
+        </ul>
     </div>
   )
 }
